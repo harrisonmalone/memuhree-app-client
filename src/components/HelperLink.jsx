661db05b1e98ccd7412413957b9d1b1c18311779
Harrison Malone <harrison.malone2@gmail.com>
2020-10-25 23:16:58 +1100

    added ternary for style depending on page

--- src/components/HelperLink.jsx
+++ src/components/HelperLink.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 const HelperLink = (props) => {
-  return <span className="helper-link">{props.children}</span>;
+  const style = props.black ? { color: "black", cursor: "none", textDecoration: "none" } : {}
+  return <span style={style} className="helper-link">{props.children}</span>;
 };
 
 export default HelperLink;
