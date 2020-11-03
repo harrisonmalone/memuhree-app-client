c81497ae92a91e51e48c0f1605ff36b891fb5574
Harrison Malone <harrison.malone2@gmail.com>
2020-11-03 16:15:33 +1100

    changed cursor

--- src/components/HelperLink.jsx
+++ src/components/HelperLink.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const HelperLink = (props) => {
-  const style = props.black ? { color: "black", cursor: "none", textDecoration: "none" } : {}
+  const style = props.black ? { color: "black", cursor: "text", textDecoration: "none" } : {}
   return <span style={style} className="helper-link">{props.children}</span>;
 };
 
